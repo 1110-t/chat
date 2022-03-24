ef04710ccb4a3dfbe0c2451097a70412761ba178
1110_t <takuya.kawaguchi.business@gmail.com>
2022-03-24 10:55:35 +0900

    make it better

--- database/migrations/2022_03_19_004906_create_comments_table.php
+++ database/migrations/2022_03_19_004906_create_comments_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamps();
             $table->string('user');
             $table->string('comment');
-            $table->string('comment');
         });
     }
 
